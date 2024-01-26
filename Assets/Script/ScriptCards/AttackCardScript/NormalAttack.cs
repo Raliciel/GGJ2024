55274782837c965ac1896948d28af4c421fcdec4
vacharavich <vacharavich@hotmail.com>
2024-01-27 01:21:26 +0700

    Fix randomizer, change normalAttack

--- Assets/Script/ScriptCards/AttackCardScript/NormalAttack.cs
+++ Assets/Script/ScriptCards/AttackCardScript/NormalAttack.cs
@@ -6,10 +6,26 @@ using UnityEngine;
 public class NormalAttack: CardSO 
 {
     public int damage = 10;
+    public int[] chance = new int[3] {10, 80, 10};
 
     public override void DoAction(Unit actor, Unit enemy) {
-        Debug.Log(actor.name + " uses NormalAttack.");
-        Debug.Log(actor.name + " damage " + damage +  " hp to " + enemy.name);
-        enemy.SetCurrentHealthPoint(enemy.GetCurrentHealthPoint() - damage);
+        int index = Randomizer.random(chance);
+        switch(index) {
+            case 0: //Miss
+                Debug.Log(actor.name + " uses NormalAttack. (Miss)");
+                break;
+
+            case 1: //Normal
+                Debug.Log(actor.name + " uses NormalAttack. (Normal)");
+                Debug.Log(actor.name + " damage " + damage +  " hp to " + enemy.name);
+                enemy.SetCurrentHealthPoint(enemy.GetCurrentHealthPoint() - damage);
+                break;
+
+            case 2: //Crit
+                Debug.Log(actor.name + " uses NormalAttack. (Crit)");
+                Debug.Log(actor.name + " damage " + (damage * 2) +  " hp to " + enemy.name);
+                enemy.SetCurrentHealthPoint(enemy.GetCurrentHealthPoint() - (damage * 2)); 
+                break;
+        }
     }
 }
