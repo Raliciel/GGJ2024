59aba1a3f9112f8eb7c39ad1d3ce52a890b9ba9d
vacharavich <vacharavich@hotmail.com>
2024-01-27 15:36:16 +0700

    remove buildingDispensor, update dejavu

--- Assets/Script/ScriptCards/DefendCardScript/DEJAVU.cs
+++ Assets/Script/ScriptCards/DefendCardScript/DEJAVU.cs
@@ -5,8 +5,30 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DEJAVU", menuName = "Card/Defend/DEJAVU", order = 1)]
 public class DEJAVU : CardSO
 {
+    public int angerCost = 5;
+    public int hpCost = 5;
+    public int recoil = 5;
+    public int reducedAnger = 15;
+    public int[] chance = new int(2) {35, 50, 15};
     public override void DoAction(Unit actor, Unit enemy)
     {
-        Debug.Log(actor.name + " uses DEJAVU.");
+        actor.payAngerCost(angerCost);
+        actor.payHPCost(hpCost);
+        int index = Randomizer.random(chance)
+
+        switch(index) {
+            case 0: //Fail
+                Debug.Log($"Despite feeling DEJA VU, {actor.name} drive too fast and crash into the fence.");
+                actor.receivedDamage(recoil);
+
+            case 1: //Deja Vu
+                Debug.Log($"While feeling DEJA VU, {actor.name} drive too fast for {enemy.name} to catch.");
+                actor.setIsDefend(true);
+
+            case 2: //Kanzen DORIFUTO
+                Debug.Log($"Kanzen DORIFUTO! With perfect drive, {actor.name} conquers the road and leaves everyone in awe.");
+                actor.setIsDefend(true);
+                enemy.reducedAnger(reducedAnger);
+        }
     }
 }
