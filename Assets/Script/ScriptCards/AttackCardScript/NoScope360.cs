853e231462bacf0e515a8ab49472831aee042909
vacharavich <vacharavich@hotmail.com>
2024-01-27 16:24:58 +0700

    Fix NoScope360

--- Assets/Script/ScriptCards/AttackCardScript/NoScope360.cs
+++ Assets/Script/ScriptCards/AttackCardScript/NoScope360.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NoScope360", menuName = "Card/Attack/NoScope360", order = 1)]
 public class NoScope360 : CardSO
 {
-    public int damage = 75;
+    public int angerCost = -15;
+    public int damage = 50;
+    public int receivedAnger = 25;
+    public int reducedAnger = 25;
+    public int[] chance = new int[2] {85, 15};
 
     public override void DoAction(Unit actor, Unit enemy)
     {
-        Debug.Log(actor.name + " use 360NoScope.");
-        Debug.Log(actor.name + " damage " + damage + " hp to " + enemy.name);
-        enemy.SetCurrentHealthPoint(enemy.GetCurrentHealthPoint() - damage);
+        actor.payAngerCost(angerCost);
+        int index = Randomizer.random(chance);
+
+        switch(index) {
+            case 0: //Miss
+                Debug.Log($"Nice attempt of 360 No Scope by {actor.name}, but he missed.");
+                actor.reducedAnger(reducedAnger);
+            
+            case 1:
+                Debug.Log($"360 No Scope into {enemy.name}'s head by {actor.name}, what a spectacular.");
+                enemy.receivedDamage(damage);
+                enemy.receivedAnger(receivedAnger);
+
+        }
     }
 }
