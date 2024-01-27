57581772b59b7191645f84efd58fcd53cd86bcb8
vacharavich <vacharavich@hotmail.com>
2024-01-27 12:56:22 +0700

    Update BodySlam, GigaMonkeBreaker

--- Assets/Script/ScriptCards/AttackCardScript/BodySlam.cs
+++ Assets/Script/ScriptCards/AttackCardScript/BodySlam.cs
@@ -5,6 +5,7 @@ public class BodySlam : CardSO
 {
     public int damage = 20;
     public int recoil = 5;
+    public int receivedAnger = 5;
     public int angerCost = 10;
     public int[] chance = new int[2]{30, 70};
 
@@ -15,16 +16,19 @@ public class BodySlam : CardSO
         
         switch(index) {
             case 0: //Miss
-                Debug.Log(actor.name + " uses BodySlam. (Miss)");
-                Debug.Log(actor.name + " damage " + recoil + " hp to " + actor.name);
-                actor.receivedDamage(recoil);
+                Debug.Log($"{actor.name} body slams the ground so hard.");
+                Debug.Log($"{actor.name} damage {recoil * 2} hp to {actor.name}");
+                actor.receivedDamage(recoil * 2);
                 break;
             
             case 1:
-                Debug.Log(actor.name + " uses BodySlam. (Normal)");
-                Debug.Log(actor.name + " damage " + damage + " hp to " + enemy.name);
-                Debug.Log(actor.name + " damage " + recoil + " hp to " + actor.name);
+                Debug.Log($"{actor.name} body slams into {enemy.name} so hard they roll altogether.");
+                
+                Debug.Log($"{actor.name} damage {damage} hp to {enemy.name}, receiving {receivedAnger} anger.");
                 enemy.receivedDamage(damage);
+                enemy.receivedAnger(receivedAnger);
+
+                Debug.Log($"{actor.name} damage {recoil} hp to {actor.name}");
                 actor.receivedDamage(recoil);
                 break;
         }
