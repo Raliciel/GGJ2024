b30c3d80ec29f00af6207f3cea72d7cd09582c48
vacharavich <vacharavich@hotmail.com>
2024-01-28 09:50:38 +0700

    fix Randomizer

--- Assets/Script/Utility/Randomizer.cs
+++ Assets/Script/Utility/Randomizer.cs
@@ -10,7 +10,7 @@ public static class Randomizer
         //[20, 25, 30, 25] => total: 100
 
         int weight_r = UnityEngine.Random.Range(0, total);
-        Debug.Log($"{weight}, Random Weight: {weight_r}");
+        Debug.Log($"{total}, Random Weight: {weight_r}");
         for(int i = 0; i < weight.Length; i++) {
             weight_r -= weight[i];
             if(weight_r < 0) return i;
